c2c381225ed32d6f6551ea20e633edf0ca84d772
sidnei paes de camargo <94030294+sidneymontanha@users.noreply.github.com>
2024-03-27 18:40:43 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+console.log('loja montanha')
+
+const getProducts = async () => {
+    const response = await fetch('js/products.json')
+    const data = await response.json()
+    return data
+}
+
+const generateCard = async () => {
+    const products = await getProducts()
+    console.log(products)
+    products.map(product => {
+        let card = document.createElement('div')
+        card.classList.add('card__produto')
+        
+        card.innerHTML = `
+            <figure>
+                <img src="images/${product.image}" alt="${product.product_name}">
+            </figure>
+            <div class="card__produto_detalhes">
+                <h4>${product.product_name}</h4>
+                <h5>${product.product_model}</h5>
+            </div>
+            <h6>${product.price}</h6>
+            `
+     const listaProdutos = document.querySelector('.lista__produtos')
+     listaProdutos.appendChild(card)
+
+    })
+}
+
+generateCard()
