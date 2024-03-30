2d9a23954408928bb393a133bd5e3eed8b37e6d9
sidnei paes de camargo <94030294+sidneymontanha@users.noreply.github.com>
2024-03-30 03:20:00 -0300

    nov commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const botaoVoltar = document.querySelector('.voltar')
+const sectionDetalhamentoproduto = document.querySelector('.produto__detalhes')
+const sectionProdutos = document.querySelector('.produtos')
+
+botaoVoltar.style.display = 'none'
+sectionDetalhamentoproduto.style.display = 'none'
+
+
 const formatCurrency = (number) => {
     return number.toLocaleString('pt-BR', {
         style: 'currency',
@@ -16,6 +24,8 @@ const generateCard = async () => {
     console.log(products)
     products.map(product => {
         let card = document.createElement('div')
+        card.id = product.id
+        console.log(card.id)
         card.classList.add('card__produto') 
         card.innerHTML = `
             <figure>
@@ -30,7 +40,47 @@ const generateCard = async () => {
      const listaProdutos = document.querySelector('.lista__produtos')
      listaProdutos.appendChild(card)
 
+     card.addEventListener('click', (e) => {
+        sectionProdutos.style.display = 'none'
+        botaoVoltar.style.display = 'block'
+        sectionDetalhamentoproduto.style.display = 'grid'
+        
+       const cardClicado = e.currentTarget
+       const idProduto = cardClicado.id
+       const produtoClicado = products.find( product => product.id == idProduto)
+        
+        preencherDadosProduto(produtoClicado)
+     })
+
     })
 }
 
 generateCard()
+
+botaoVoltar.addEventListener('click', () => {
+    sectionProdutos.style.display = 'flex'
+        botaoVoltar.style.display = 'none'
+        sectionDetalhamentoproduto.style.display = 'none'
+})
+
+const preencherDadosProduto = (product) => {
+    const images = document.querySelectorAll('.produto__detalhes_imagens figure img')
+    const imagesArray = Array.from(images)
+    imagesArray.map( image => {
+        image.src = `./images/${product.image}`
+    })
+
+    document.querySelector('.detalhes h4').innerHTML = product.product_name
+    document.querySelector('.detalhes h5').innerHTML = product.product_model
+    document.querySelector('.detalhes h6').innerHTML = formatCurrency(product.price)
+    
+}
+
+const details = document.querySelector('details')
+
+details.addEventListener('toggle', () => {
+    const summary = document.querySelector('summary')
+    summary.classList.toggle('icone-expandir')
+    summary.classList.toggle('icone-recolher')
+})
+
